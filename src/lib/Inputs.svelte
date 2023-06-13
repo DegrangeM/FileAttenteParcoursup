0b0e17eafff9d0e4316b8dd2aea73450af777cea
DegrangeM <53106394+DegrangeM@users.noreply.github.com>
2023-06-13 02:08:07 +0200

    Update Inputs.svelte

--- src/lib/Inputs.svelte
+++ src/lib/Inputs.svelte
@@ -42,66 +42,60 @@
                 scrollTo: true,
             },
         });
+        const PREVIOUS = {
+            text: "Précédent",
+            action: tour.back,
+        };
+        const NEXT = {
+            text: "Suivant",
+            action: tour.next,
+        };
+        const END = {
+            text: "Terminer",
+            action: tour.complete,
+        };
         tour.addStep({
             text: `La formation a classé chaque candidature selon ses critères.<br />
                 Votre position dans ce classement correspond à votre rang dans la liste d'appel.<br />
                 Cette position est fixe et ne changera pas.`,
-            buttons: [
-                {
-                    text: "Suivant",
-                    action: tour.next,
-                },
-            ],
+            buttons: [NEXT],
         });
         tour.addStep({
             text: `Il y a devant vous :
             <ul>
-                <li>${$rouge} Personnes ayant reçu une proposition d'admission mais l'ayant refusé</li>
-                <li>${$vertjaune} Personnes ayant reçu une proposition d'admission et l'ayant soit accepté, soit pas encore accepté ou refusé</li>
-                <li>...</li>
+                <li><strong>${$rouge} Personnes ayant reçu une proposition d'admission mais l'ayant refusé</strong></li>
+                <li><strong>${$vertjaune} Personnes ayant reçu une proposition d'admission et l'ayant soit accepté, soit pas encore accepté ou refusé</strong></li>
+                <li>${$bleu1} Personnes en liste d'attente devant vous</li>
+                <li>${$orange1} Personnes qui ont abandonnés leur place en file d'attente et qui étaient devant vous</li>
             </ul>`,
             attachTo: {
                 element: ".zone1",
                 on: "bottom",
             },
-            buttons: [
-                {
-                    text: "Suivant",
-                    action: tour.next,
-                },
-            ],
+            buttons: [PREVIOUS, NEXT],
         });
         tour.addStep({
-            text: `
+            text: `Il y a devant vous :
                 <ul>
-                    <li>...</li>
-                    <li>${$bleu1} Personnes en liste d'attente devant vous</li>
-                    <li>${$orange1} Personnes qui ont abandonnés leur place en file d'attente et qui étaient devant vous</li>
+                    <li>${$rouge} Personnes ayant reçu une proposition d'admission mais l'ayant refusé</li>
+                    <li>${$vertjaune} Personnes ayant reçu une proposition d'admission et l'ayant soit accepté, soit pas encore accepté ou refusé</li> 
+                    <li><strong>${$bleu1} Personnes en liste d'attente devant vous</strong></li>
+                    <li><strong>${$orange1} Personnes qui ont abandonnés leur place en file d'attente et qui étaient devant vous</strong></li>
                 </ul>`,
             attachTo: {
                 element: ".zone2",
                 on: "bottom",
             },
-            buttons: [
-                {
-                    text: "Suivant",
-                    action: tour.next,
-                },
-            ],
+            buttons: [PREVIOUS, NEXT],
         });
         tour.addStep({
             text: `${$rouge} + ${$vertjaune} + ${$bleu1} + ${$orange1} = ${
-                $rouge + $vertjaune + $bleu1 + $orange1
+                $position_liste_appel - 1
             }<br />
             Il y a donc  ${
-                $rouge + $vertjaune + $bleu1 + $orange1
+                $position_liste_appel - 1
             } personnes devant vous et vous êtes donc ${$position_liste_appel}ème sur la liste d'appel.`,
-            buttons: [
-                {
-                    text: "Suivant",
-                    action: tour.next,
-                },
-            ],
+            buttons: [PREVIOUS, END],
         });
         tour.start();
     }
@@ -114,36 +108,62 @@
                 scrollTo: true,
             },
         });
+        const PREVIOUS = {
+            text: "Précédent",
+            action: tour.back,
+        };
+        const NEXT = {
+            text: "Suivant",
+            action: tour.next,
+        };
+        const END = {
+            text: "Terminer",
+            action: tour.complete,
+        };
         tour.addStep({
             text: `
-                Les personnes dans la file d'appel qui n'ont pas encore reçu de proposition d'admission sont placées en liste d'attente.<br />
+                La formation a classé chaque candidature selon ses critères.<br />
+                Les personnes qui n'ont pas encore reçu de proposition d'admission sont placées en liste d'attente.<br />
                 Votre position dans cette liste d'attente va diminuer au fur et à mesure que des personnes placées
                 devant vous vont recevoir des propositions d'admission ou abandonner leur place en liste d'attente.`,
-            buttons: [
-                {
-                    text: "Suivant",
-                    action: tour.next,
-                },
-            ],
+            buttons: [NEXT],
         });
         tour.addStep({
-            text: `Initialement, il y avait ${$position_liste_appel - 1} personnes devant vous. Parmi ces personnes, il y en a :<br />
+            text: `Initialement, il y avait ${
+                $position_liste_appel - 1
+            } personnes devant vous. Parmi ces personnes, il y en a :<br />
+                <ul>
+                    <li><strong>${$rouge} qui ont reçu une proposition d'admission mais l'ont refusé</strong></li>
+                    <li><strong>${$vertjaune} qui ont reçu une proposition d'admission et l'ont soit accepté, soit pas encore accepté ou refusé</strong></li>
+                    <li>${$orange1} qui ont abandonnés leur place en file d'attente</li>
+                </ul>`,
+            attachTo: {
+                element: ".zone1",
+                on: "bottom",
+            },
+            buttons: [PREVIOUS, NEXT],
+        });
+        tour.addStep({
+            text: `Initialement, il y avait ${
+                $position_liste_appel - 1
+            } personnes devant vous. Parmi ces personnes, il y en a :<br />
                 <ul>
                     <li>${$rouge} qui ont reçu une proposition d'admission mais l'ont refusé</li>
                     <li>${$vertjaune} qui ont reçu une proposition d'admission et l'ont soit accepté, soit pas encore accepté ou refusé</li>
-                    <li>${$orange1} qui ont abandonnés leur place en file d'attente</li>
-                </ul>
-                Il y a donc ${$bleu1} personnes devant vous et vous êtes donc ${$position_liste_attente}ème sur la liste d'attente.`,
+                    <li><strong>${$orange1} qui ont abandonnés leur place en file d'attente</strong></li>
+                </ul>`,
             attachTo: {
                 element: ".zone2",
                 on: "bottom",
             },
-            buttons: [
-                {
-                    text: "Suivant",
-                    action: tour.next,
-                },
-            ],
+            buttons: [PREVIOUS, NEXT],
+        });
+        tour.addStep({
+            text: `${
+                $position_liste_appel - 1
+            } - ${$rouge} - ${$vertjaune} - ${$orange1} = ${$bleu1}<br />
+                Il y a donc ${$bleu1} personnes devant vous et vous êtes donc ${$position_liste_attente}ème sur la liste d'attente.`,
+            buttons: [PREVIOUS, END],
         });
         tour.start();
     }
@@ -158,7 +178,8 @@ Nombre de places dans la formation : <input
 Votre position dans la liste d'attente :
 <input type="number" bind:value={$position_liste_attente} min="0" /><br />
 Nombre total de candidats dans la liste d'attente :
-<input type="number" bind:value={$candidats_liste_attente} min="0" /> <a href="#" on:click={startAttenteTour}>(?)</a><br />
+<input type="number" bind:value={$candidats_liste_attente} min="0" />
+<a href="#" on:click={startAttenteTour}>(?)</a><br />
 
 Votre position dans la liste d'appel de la formation :
 <input type="number" bind:value={$position_liste_appel} min="0" />
